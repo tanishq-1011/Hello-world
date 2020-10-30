995c9574b91058b627cac809167df9c78a57255f
Tanishq More <44018380+tanishq-1011@users.noreply.github.com>
2020-10-30 23:21:27 +0530

    Update AddNum.py

--- Python/AddNum.py
+++ Python/AddNum.py
@@ -1,8 +1,8 @@
 # Adds four values
-num1 = input("Number 1: ")
-num2 = input("Number 2: ")
-num3 = input("Number 3: ")
-num4 = input("Number 4: ")
+num1 = float(input("Number 1: "))
+num2 = float(input("Number 2: "))
+num3 = float(input("Number 3: "))
+num4 = float(input("Number 4: "))
 
 sum = float(num1) + float(num2) + float(num3) + float(num4)
 
